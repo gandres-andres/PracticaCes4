c42c7ea5f897cfcb626c5688647ae763cef85bad
gabo97 <tolosa-321@hotmail.com>
2018-03-21 12:48:41 -0500

    socket change

--- socket/back/server.js
+++ socket/back/server.js
@@ -4,15 +4,40 @@ var io = require('socket.io')(http);
 
 var messages = [];
 
+function getMessage(channel) {
+  var mensajes = [];
+  for (var i = 0; i < messages.length; i++) {
+    if (messages[i].channel === channel) {
+      mensajes.push(messages[i]);
+    }
+  }
+  return mensajes;
+}
+
 io.on('connection', socket => {
   console.log('a user connected');
-  io.emit('message', messages);
+
+  var channel = '1';
+  io.emit('messages', getMessage(channel));
+
   socket.on('disconnect', () => {
     console.log('user disconnected');
   });
-  socket.on('message', msg => {
+
+  socket.on('new-message', msg => {
     messages.push(msg);
-    io.emit('message', messages);
+    io.emit('messages', getMessage(msg.channel));
+  });
+
+  socket.on('change channel', function (newChannel) {
+    if (newChannel != channel) {
+      socket.leave(channel);
+      socket.join(newChannel);
+      channel = newChannel;
+    }
+    if (getMessage(channel) != null) {
+      io.emit('messages', getMessage(channel));
+    }
   });
 });
 
--- socket/front/src/app/socket/socket.component.ts
+++ socket/front/src/app/socket/socket.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import * as socketIo from 'socket.io-client';
+import { Channel } from '../models/channel.model';
 
 @Component({
   selector: 'app-socket',
@@ -7,22 +8,34 @@ import * as socketIo from 'socket.io-client';
   styleUrls: ['./socket.component.css']
 })
 export class SocketComponent implements OnInit {
+  channels: Channel[];
+  channel: string;
   message: '';
+
   messages = [];
   socket = socketIo('http://localhost:3000');
 
   constructor() { }
 
   ngOnInit() {
-    this.socket.on('message', (data) => {
-      console.log('mensaje',data)
-      this.messages= data;
+    this.socket.on('messages', (data) => {
+      this.messages = data;
     });
+    
+    this.channels = [
+      { id: 1, name: 'channel 1' },
+      { id: 2, name: 'channel 2' },
+      { id: 3, name: 'channel 3' }
+    ];
+    this.channel = '1';
   }
 
   sendMessage() {
-    this.socket.emit('message', this.message);
+    this.socket.emit('new-message', { message: this.message, channel: this.channel });
     this.message = '';
   }
 
+  changeChannel() {
+    this.socket.emit('change channel', this.channel);
+  }
 }
